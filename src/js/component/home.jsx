bf316076526c0edc231e3b5d9d8d17e9783d08e3
Facundo <facundogds@gmail.com>
2022-06-29 01:02:31 +0000

    A little more sophisticated

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,18 +4,33 @@ import React, { useState } from "react";
 const Home = () => {
   //I set a state: a variable which is a state and can change.
   let [counter, setCounter] = useState(10); //By default is 10, when the browser loads.
+  const [amount, setAmount] = useState(1);
+
   const increaseHandler = () => {
-    setCounter((prevState) => prevState + 1);
+    setCounter((prevState) => prevState + amount);
   };
   const decreaseHandler = () => {
-    setCounter((prevState) => prevState - 1);
+    setCounter((prevState) => prevState - amount);
   };
+//React automatically assigns an event to the listener.
+  const amountHandler = (event) => {
+    setAmount(parseInt(event.target.value));
+  };
+
   return (
     <div className="mg">
       <h1>So this is my counter...</h1>
-	  <h2>The current value is {counter}.</h2>
-	  <button onClick={decreaseHandler}>Decrease</button>&nbsp;
-	  <button onClick={increaseHandler}>Increase</button>
+      <h2>The current value is {counter}.</h2>
+      <label htmlFor="amount">Modify by&nbsp;</label>
+      <input
+        id="amount"
+        type="number"
+        value={amount}
+        onChange={amountHandler} //Listener.
+      />
+      &nbsp;
+      <button onClick={decreaseHandler}>Decrease</button>&nbsp;
+      <button onClick={increaseHandler}>Increase</button>
     </div>
   );
 };
